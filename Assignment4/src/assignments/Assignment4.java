c2833a1847bda7003ee393c01693bf5801f924ed
varunreddy <varunreddy6666@yahoo.com>
2016-06-03 20:17:27 +0530

    fourth assignment

---
+++ Assignment4/src/assignments/Assignment4.java
@@ -0,0 +1,17 @@
+package assignments;
+
+public class Assignment4 {
+
+	public static void main(String[] args) 
+	{
+		int x=5,y=10;
+		
+		System.out.println(x + y * 2);
+		System.out.println(x - y + 2);
+		System.out.println((x+y) * 2);
+		System.out.println(y % x);
+		
+		
+	}
+
+}
